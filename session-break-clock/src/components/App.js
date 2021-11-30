7f6a0d0fa91561202fd60c48897d788b6c2da8fb
ramabadash <89576646+ramabadash@users.noreply.github.com>
2021-11-30 13:17:47 +0200

    # start /stop + reset  functionality- test not pass

--- session-break-clock/src/components/App.js
+++ session-break-clock/src/components/App.js
@@ -1,26 +1,40 @@
 import React, { useState } from 'react';
 import TimeController from './TimeController';
 import Timer from './Timer';
+import '../styles/App.css';
 
 export default function App() {
   /***** STATES *****/
   const [breakLength, setBreakLength] = useState(5);
   const [sessionLength, setSessionLength] = useState(25);
   const [currentStage, setCurrentStage] = useState('session');
-  const [timer, setTimer] = useState(25 * 60);
 
   /***** FUNCTIONS *****/
-  const reset = () => {
+  const resetLength = () => {
     setSessionLength(25);
     setBreakLength(5);
   };
 
   return (
-    <div>
-      <h1>25+5 clock</h1>
-      <TimeController name={'break'} setTime={setBreakLength} currentTime={breakLength} /> {/* Break */}
-      <TimeController name={'session'} setTime={setSessionLength} currentTime={sessionLength} /> {/* Session */}
-      <Timer currentStage={currentStage} sessionLength={sessionLength} breakLength={breakLength} />
-    </div>
+    <>
+      <header>
+        <h1>
+          25+5 clock <i className='fas fa-clock'></i>
+        </h1>
+        <h2>It's time to start</h2>
+      </header>
+      <div className='app-container'>
+        <div className='controller-app'>
+          <TimeController name={'break'} setTime={setBreakLength} currentTime={breakLength} /> {/* Break */}
+          <TimeController name={'session'} setTime={setSessionLength} currentTime={sessionLength} /> {/* Session */}
+        </div>
+        <Timer
+          currentStage={currentStage}
+          sessionLength={sessionLength}
+          breakLength={breakLength}
+          resetLength={resetLength}
+        />
+      </div>
+    </>
   );
 }
--- session-break-clock/src/components/Timer.js
+++ session-break-clock/src/components/Timer.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import '../styles/Timer.css';
 
-export default function Timer({ currentStage, sessionLength, breakLength }) {
+export default function Timer({ currentStage, resetLength, sessionLength, breakLength }) {
+  /***** STATES *****/
+  const [timer, setTimer] = useState(currentStage === 'session' ? sessionLength * 60 : breakLength * 60);
+  const [isRunning, setIsRunning] = useState(false);
+  const [intervalName, setIntervalName] = useState(null);
+
   /***** FUNCTIONS *****/
+  //Update timer state on every length or stage change
+  useEffect(() => {
+    if (!isRunning) {
+      setTimer(currentStage === 'session' ? sessionLength * 60 : breakLength * 60);
+    }
+  }, [sessionLength, breakLength, currentStage]);
+
   //Convert time to mm : ss format
   const clockify = (time) => {
     let minutes = Math.floor(time / 60);
@@ -11,22 +24,47 @@ export default function Timer({ currentStage, sessionLength, breakLength }) {
     return minutes + ':' + seconds;
   };
 
+  //Run and stop time
+  const startStopTime = () => {
+    if (!isRunning) {
+      //Update clock every second
+      setIntervalName(
+        setInterval(() => {
+          if (timer > 0) {
+            setTimer((prevTimer) => prevTimer - 1);
+          } else {
+            clearInterval(intervalName); //Stop clock
+            setIsRunning(false);
+          }
+        }, 1000)
+      );
+      setIsRunning(true);
+      return;
+    } else {
+      clearInterval(intervalName); //Stop clock
+      setIsRunning(false);
+    }
+  };
+
+  const reset = () => {
+    resetLength();
+    clearInterval(intervalName); //Stop clock
+    setIsRunning(false);
+    setTimer(currentStage === 'session' ? sessionLength * 60 : breakLength * 60);
+  };
+
   return (
-    <div>
-      <div>
-        <label id='timer-label'>{currentStage}</label>
-        <label id='time-left'>
-          {' '}
-          {/* TODO - Pass test*/}
-          {currentStage === 'session' ? clockify(sessionLength * 60) : clockify(breakLength * 60)}
-        </label>
+    <div className='timer-container'>
+      <div className='time-labels'>
+        <label id='timer-label'>{currentStage} Time!</label>
+        <label id='time-left'>{clockify(timer)}</label>
       </div>
-      <div className='btn-container'>
+      <div className='btn-container' onClick={startStopTime}>
         <span id='start_stop'>
           <i className='fas fa-play'></i>
           <i className='fas fa-pause'></i>
         </span>
-        <span id='reset'>
+        <span id='reset' onClick={reset}>
           <i className='fas fa-square'></i>
         </span>
       </div>
